033a430c1d2c27db470a1a160f33af59340b4052
Pedro Henriques <pedro@pedrojhenriques.com>
2023-02-07 01:49:58 +0000

    added swarm files

---
+++ cli/swarm_run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+mkdir -p data/;
+
+if [ -f "./.env" ]
+then
+	set -o allexport;
+  . ./.env;
+  set +o allexport
+fi
+
+docker compose -f ./docker-stack.yml build --pull;
+docker compose -f ./docker-stack.yml pull --ignore-pull-failures;
+
+docker swarm init || true;
+
+docker stack deploy -c ./docker-stack.yml docker-coaching;
